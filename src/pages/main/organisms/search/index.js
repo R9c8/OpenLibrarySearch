388fb5e36ca2b9c7710e09dd8c3f1eb98ccd9615
R9c8 <alkanenko@ya.ru>
2021-05-23 23:54:04 +0300

    added some search views

--- src/pages/main/organisms/search/index.js
+++ src/pages/main/organisms/search/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component} from 'react';
+
+import Down from "./down.svg";
 
 import './styles/index.css';
 
@@ -8,16 +10,67 @@ export const Search = () => (
     <div className="search-container">
         <SearchInput />
         <SearchBox />
+        <SearchLimitationButton />
+        <SearchLimitationSelect />
     </div>
 )
 
-const SearchBox = () => (
-    <div className="search-box">
-        <SearchResult />
-        <SearchResult />
-        <SearchResult />
+const SearchLimitationButton = () => {
+    const limitation = 'Author';
+    if (limitation === 'Author') {
+        return (
+            <div className="search-limitation-button">
+                <div>Author</div>
+                <div><img src={Down} /></div>
+            </div>
+        )
+    } else if (limitation === 'Title') {
+        return (
+            <div className="search-limitation-button">
+                <div>Title</div>
+                <div><img src={Down} /></div>
+            </div>
+        )        
+    } else if (limitation === 'All') {
+        return (
+            <div className="search-limitation-button">
+                <div>All</div>
+                <div><img src={Down} /></div>
+            </div>
+        )        
+    }
+};
+
+const SearchLimitationSelect = () => (
+    <div className="search-limitation-select">
+        <SearchLimitationSelectChoise value="All" />
+        <div style={{ width: '100%', height: '1px', backgroundColor: '#e5e5e5'}} />
+        <SearchLimitationSelectChoise value="Title" />
+        <div style={{ width: '100%', height: '1px', backgroundColor: '#e5e5e5'}} />
     </div>
-)
+);
+
+const SearchLimitationSelectChoise = ({value}) => (
+    <div className="search-limitation-select-choice">
+        { value }
+    </div>
+);
+
+const SearchBox = () => {
+    const isVisible = false;
+    if (isVisible) {
+        return (
+            <div className="search-box">
+                <SearchResult />
+                <SearchResult />
+                <SearchResult />
+            </div>
+        )
+    } else {
+        return (<div />)
+    }
+      
+}
 
 const SearchResult = () => (
     <div>
